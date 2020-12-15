f339c011356871248d222a65c642de23960088f7
Miika Moilanen <miika.moilanen@tietoevry.com>
2020-12-15 09:16:50 +0200

    Added hard mode

--- +bindings.el
+++ +bindings.el
@@ -22,7 +22,6 @@
 ;; (defun insert-python-breakpoint ()
 ;;   "Adds a IPython breakpoint to where the cursor is."
 ;;   (insert "from IPython.core.debugger import set_trace; set_trace()"))
-
 (map! :after python
       :map python-mode-map
       :localleader
@@ -42,6 +41,7 @@
       ;;  :desc "Format buffer" "a" #'py-yapf-buffer)
       )
 
+
 ;; (map! :after smartparens-mode
 ;;       :map paredit-mode-map
 ;;       (:prefix "k"
@@ -57,3 +57,37 @@
 
 (global-set-key (kbd "C-S-C") #'copy-to-clipboard)
 (global-set-key (kbd "C-S-V") #'paste-from-clipboard)
+
+
+;; (semi) HARD MODE
+;; (define-minor-mode my-override-mode
+;;   "Overrides all major and minor mode keys" t)
+
+;; (defvar my-override-map (make-sparse-keymap "my-override-map")
+;;   "Override all major and minor mode keys")
+
+;; (add-to-list 'emulation-mode-map-alists
+;;   `((my-override-mode . ,my-override-map)))
+
+;; (define-key my-override-map (kbd "<left>")
+;;   (lambda ()
+;;     (interactive)
+;;     (message "Use Vim keys: h for Left")))
+
+;; (define-key my-override-map (kbd "<right>")
+;;   (lambda ()
+;;     (interactive)
+;;     (message "Use Vim keys: l for Right")))
+
+;; (define-key my-override-map (kbd "<up>")
+;;   (lambda ()
+;;     (interactive)
+;;     (message "Use Vim keys: k for Up")))
+
+;; (define-key my-override-map (kbd "<down>")
+;;   (lambda ()
+;;     (interactive)
+;;     (message "Use Vim keys: j for Down")))
+
+;; (evil-make-intercept-map my-override-map)
+;; HARD MODE END
